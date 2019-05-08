5f243d6bcd4a1a2a799a218a851b1651ca62ad3f
valeria campilongo <cvaleriac@hotmail.it>
2019-05-08 15:44:22 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,4 +1,5 @@
 module SongsHelper
+<<<<<<< HEAD
   def artist_select(song, artist)
     if artist
       hidden_field_tag "song[artist_id]", song.artist_id
@@ -6,4 +7,14 @@ module SongsHelper
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     end
   end
+=======
+  def artist_id_field(song)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      hidden_field_tag "song[artist_id]", song.artist_id
+    end
+  end
+
+>>>>>>> 5f26fa0006757f2541b7df100190d422b07351d0
 end
